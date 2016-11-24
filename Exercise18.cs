bc5f45e2dde83aefe234ac8be735ef3c2c0a887f
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-24 18:58:20 +0200

    Add files via upload

---
+++ Exercise18.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Array
+{
+    class Exercise18
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            int[,] arr = new int[2,2];
+
+            Console.Write(" Input elements in the matrix: ");
+
+            for (x = 0; x < 2; x++)
+            {
+                for(y=0; y<2; y++)
+                {
+                    Console.Write("\nElement - [{0},{1}] : ", x, y);
+                    arr[x, y] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.Write(" The matrix is: ");
+
+            for (x = 0; x < 2; x++)
+            {
+                Console.Write("\n");
+                for (y=0; y<2; y++)
+                {
+                    Console.Write("  {0} ", arr[x, y]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
