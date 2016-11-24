99b1c9723b62b3fa890029ffe16e831b8500e27e
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-24 18:30:43 +0200

    Add files via upload

---
+++ Exercise16.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Array
+{
+    class Exercise16
+    {
+        static void Main(string[] args)
+        {
+            int i, j=0, x, aa, a;
+            int[] arr = new int[10];
+
+            Console.Write("Input the number of elements to be stored in the array: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Input {0} elements in the array: ", x);
+
+            for (i = 0; i < x; i++)
+            {
+                Console.Write("\nElement - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+             aa = 0;
+
+            for(i=0; i < x; i++)
+            {
+                if (aa < arr[i])
+                {
+                    aa = arr[i];
+                    j = i;
+                }
+            }
+            a = 0;
+
+            for (i = 0; i < x; i++)
+            {
+                if (i == j)
+                {
+                    i++; j--;
+                }
+                else
+                {
+                    if (a < arr[i])
+                        a = arr[i];
+                }
+            }
+            Console.Write(" The second largest element in the array is: {0}",a);
+
+            Console.ReadLine();
+        }
+    }
+}
