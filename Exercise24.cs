cd3fb7a3b70f506c9bc129a2257f82c40a4383ef
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-25 10:32:35 +0200

    Add files via upload

---
+++ Exercise24.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Array
+{
+    class Exercise24
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, sum=0, w=0;
+            int[,] arr = new int[10, 10];
+
+            Console.Write("Input the size: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            w = z;
+
+            Console.Write(" Input elements in the matrix: ");
+            for (x = 0; x < z; x++)
+            {
+                for (y = 0; y < z; y++)
+                {
+                    Console.Write("\nElement - [{0}],[{1}] : ", x, y);
+                    arr[x, y] = Convert.ToInt32(Console.ReadLine());
+                    }
+            }
+
+            Console.Write(" The matrix is: ");
+            for (x = 0; x < z; x++)
+            {
+                Console.Write("\n");
+                for (y = 0; y < z; y++)
+                    Console.Write("  {0} ", arr[x, y]);
+            }
+            for (x = 0; x < z; x++)
+            {
+                w -= 1;
+                for (y = 0; y < z; y++)
+                {
+                    if (w == y)
+                        sum += arr[x, y];
+                }
+            }
+
+            Console.Write("\nAddition of the left diagonal elements is: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
